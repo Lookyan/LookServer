e2e474ea4c3a944ea4f59740e11381afabb5ad70
Alex <digwnews@gmail.com>
2015-10-21 12:55:23 +0300

    makefile

--- main.cpp
+++ main.cpp
@@ -44,7 +44,6 @@ int main(int argc, char **argv)
     }
     catch(std::exception& err) {
         std::cout << err.what() << std::endl;
-        delete httpServer;
     }
     
     delete httpServer;
